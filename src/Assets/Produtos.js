47831ee5df8144ef96ab58eebb4392334df5e82a
Breno <brenosousacst@.com>
2023-02-26 23:19:58 -0300

    subindo estrutura

---
+++ src/Assets/Produtos.js
@@ -0,0 +1,112 @@
+export const produtos = [
+    {
+        id: 1,
+        produto: "Whey Protein 1kg",
+        src: '',
+        descricao: "Ajuda na hipertrofia",
+        preco: 99.00,
+        Estoque: 3,
+        classificacao: 5,
+        classificacoes: 3,
+
+    },
+    {
+        id: 2,
+        produto: "Barra de proteina",
+        src: '',
+        descricao: "Alto valor de fibras",
+        preco: 28.00,
+        Estoque: 7,
+        classificacao: 4,
+        classificacoes: 8,
+
+    },
+    {
+        id: 3,
+        produto: "Colageno",
+        src: '',
+        descricao: "Aumento Volume Muscular",
+        preco: 29.00,
+        Estoque: 9,
+        classificacao: 4,
+        classificacoes: 7,
+
+    },
+    {
+        id: 4,
+        produto: "Colageno Tipo 2",
+        src: '',
+        descricao: "Reparação muscular",
+        preco: 60.00,
+        Estoque: 2,
+        classificacao: 5,
+        classificacoes: 3,
+
+    },
+    {
+        id: 5,
+        produto: "Medium Whey 1kg",
+        src: '',
+        descricao: "Ajuda na hipertrofia",
+        preco: 99.00,
+        Estoque: 3,
+        classificacao: 5,
+        classificacoes: 3,
+
+    },
+    {
+        id: 6,
+        produto: "Proteína de carne 1kg",
+        src: '',
+        descricao: "Ajuda na hipertrofia",
+        preco: 120.00,
+        Estoque: 3,
+        classificacao: 5,
+        classificacoes: 12,
+
+    },
+    {
+        id: 7,
+        produto: '',
+        src: produto7,
+        descricao: "Ajuda na hipertrofia",
+        preco: 75.00,
+        Estoque: 12,
+        classificacao: 5,
+        classificacoes: 8,
+
+    },
+    {
+        id: 8,
+        produto: "Rice Protein 1kg",
+        src: '',
+        descricao: "Ajuda na hipertrofia",
+        preco: 75.00,
+        Estoque: 4,
+        classificacao: 4,
+        classificacoes: 6,
+
+    },
+    {
+        id: 9,
+        produto: "Rice protein 1kg",
+        src: '', 
+        descricao: "Ajuda na hipertrofia",
+        preco: 78.00,
+        Estoque: 3,
+        classificacao: 5,
+        classificacoes: 3,
+
+    },
+    {
+        id: 10,
+        produto: "Whey Protein 1kg",
+        src: '',
+        descricao: "Ajuda na hipertrofia",
+        preco: 99.00,
+        Estoque: 3,
+        classificacao: 5,
+        classificacoes: 3,
+
+    }
+]
